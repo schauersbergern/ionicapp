45e0d746bb52415ffb9dd1cc4e4dcf1e82ec6a72
Marc Menghin <marc.menghin@gmail.com>
2019-06-14 17:57:13 +0200

    add guid package

--- src/services/MeetupService.ts
+++ src/services/MeetupService.ts
@@ -1,9 +1,26 @@
 import { Injectable } from "@angular/core";
+import { MeetupModel } from "./model/MeetupModel";
+import { UserService } from "./UserService";
+import { Guid } from "guid-typescript"
 
 @Injectable()
 export class MeetupService {
 
     constructor(
+            private userService : UserService,
+        ) {}
         
-		) {}
+
+        public create(locationId : string): MeetupModel
+        {
+            let meetup = new MeetupModel();
+
+            meetup.closed = false;
+            meetup.id = Guid.create().toString();
+            meetup.locationId = locationId;
+            meetup.startedAt = new Date();
+            meetup.startedByUserId = this.userService.currentUserId;
+
+            return meetup;
+        }
 }
