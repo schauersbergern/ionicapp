0aad90aed804e83a58f7d356a0b577bbff17ac0c
Marc Menghin <marc.menghin@gmail.com>
2019-06-15 12:57:32 +0200

    fix missing get method

--- src/services/MeetupService.ts
+++ src/services/MeetupService.ts
@@ -1,13 +1,16 @@
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
 import { MeetupModel } from '../services/model/MeetupModel';
+import { MeetupRepository } from './repositories/MeetupRepository';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetupService {
 
-  constructor(private firestore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore,
+              private meetupRepository: MeetupRepository) { }
 
   getAllMeetups() {
     return this.firestore.collection('meetupList').snapshotChanges();
@@ -15,5 +18,9 @@ export class MeetupService {
 
   addMeetup(meetupModel : MeetupModel) {
     this.firestore.collection('meetupList').add(meetupModel);
+  }
+
+  get(id: string) : Observable<MeetupModel> {
+    return this.meetupRepository.read(id);
   }
 }
